b5aea5319ea1d05167a7da4b60b503b250afba1e
xup60521 <84887662+xup60521@users.noreply.github.com>
2024-09-08 13:39:13 +0800

    finish desktop layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,12 @@ export default {
     theme: {
         extend: {
             colors: {
-
+                c_Violet: "hsl(257, 40%, 49%)",
+                c_Soft_Magenta: "hsl(300, 69%, 71%)",
             },
             fontFamily: {
-
+                headings: ["Poppins", "sans-serif"],
+                body: [ "Open Sans", "system-ui"]
             }
         },
     },
